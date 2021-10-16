f94b508063801bf32fe407460d68e50ab5d4a94c
FlamesX128 <glowingorangeflames@gmail.com>
2021-10-15 22:58:16 -0500

    NextLog

--- index.d.ts
+++ index.d.ts
@@ -1,62 +1,6 @@
 declare module "@flamesx128/nextlog" {
   export type TypeLog = "ERR" | "OK" | "WARN";
 
-  /**
- * Get current time.
- * @returns {[string, string]}
- */
-  export function getCurrentTime(): [string, string] {
-    const date = new Date();
-
-    return [
-      buildClosers(
-        `${date.toLocaleDateString()}`
-      ),
-
-      buildClosers(
-        `${date.toLocaleTimeString()}`
-      )
-    ];
-  }
-
-
-  /**
-   * Gets current status.
-   * @param {TypeLog} type
-   * @returns {string}
-   */
-  export function getCurrentStatus(type: TypeLog): string {
-    return buildClosers(
-      validLogs[type](type)
-    );
-  }
-
-
-  /**
-   * Add brackets to the string.
-   * @param {string} str - Value to add brackets.
-   * @returns {string}
-   */
-  export function buildClosers(str: string): string {
-    return grey("[") + str + grey("]");
-  }
-
-
-  /**
-   * 
-   * @param {TypeLog} type - Status type.
-   * @param {any} message - Message to print.
-   * @param {any[]} optionalParams - Optional params.
-   * @returns {void}
-   */
-  export function buildTypeLog(type: TypeLog, message: any, ...optionalParams: any[]): void {
-    console.log(
-      `${getCurrentTime().join(" ")} ${getCurrentStatus(type)} ${message}`,
-      optionalParams[0].length ? optionalParams[0] : ""
-    );
-  }
-
-
   export = {
     /**
      * Print a console log with the status "ERR"
